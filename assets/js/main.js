14c0b5346ee25eea794d4a57eef1495bc919bf9b
DEEP DUBEY <64470309+Deep-Dubey@users.noreply.github.com>
2024-01-28 14:53:26 +0530

    fourth  time uploafing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+const scrollProgress = document.getElementById('scroll_progress_bar');
+const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
 (function ($) {
     'use strict';
 
